976624d8ec55f7254f8ee66b4504598d19202da0
Adi-Pranowo <nbm.edp.it@gmail.com>
2025-07-24 02:04:57 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -9,7 +9,9 @@ def home():
 
 @app.route('/signature', methods=['POST'])
 def signature():
-    data = request.get_json(force=True)
+    data = request.get_json(force=True, silent=True)
+    if not data:
+        data = request.form or request.values or None
     if not data:
         return jsonify({"error": "No JSON body received"}), 400
 
